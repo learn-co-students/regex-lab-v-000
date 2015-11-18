f625248295fa9247126621e8d8b0e9695825cab6
selichter <lichter.sarahe@gmail.com>
2015-11-18 09:25:43 -0600

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,27 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\A[aeiou]/i)
+    return true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.scan(/\A[A-Z].+[[:punct:]]\z/).any? 
 end
 
-def valid_phone_number?(phone)
-
+def valid_phone_number(phone)
+  if phone.scan(/\d/).length==10
+    return true
+  else
+    false
+  end
 end
