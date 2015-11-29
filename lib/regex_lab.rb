0f718c10768e803d87ce21e5e0f4ebe92e172510
lisamarie616 <lisamarie616@gmail.com>
2015-11-29 10:21:16 -0800

    revise methods using Regex with #match and #scan

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/[uU][nN]\w*[iI][nN][gG]/)
+  text.scan(/[uU][nN]\w*[iI][nN][gG]\b/)
 end
 
 def words_five_letters_long(text)
@@ -11,7 +11,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/^[A-Z].+\W$/) ? true : false
+  text.match(/^[A-Z].+[\.!?]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
