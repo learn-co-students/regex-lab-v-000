43cc243050443fc8b57d050266b68e731a7a5d69
authorbeard <msnolan@gmail.com>
2015-12-02 15:01:24 -0500

    all tests passing

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,21 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+  word.match(/\b[aeiou]|[AEIOU]/) != nil ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)  
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
-end
+  text.match(/^[A-Z].*\W$/) == nil ? false : true
+ end
 
 def valid_phone_number?(phone)
-
+  phone.match(/\W?\d{3}\W?\d{3}\W?\d{4}\b/) == nil ? false: true
 end
