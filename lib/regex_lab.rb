75e1960d4659ac4282a3c1e52e0980da8e7a059c
lizdenhup <lizdenhup@gmail.com>
2015-12-18 22:01:20 -0600

    finished regex lab

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,30 @@
-def starts_with_a_vowel?(word)
+# require 'pry'
 
+def starts_with_a_vowel?(word)
+    !!word.match(/\b[aeiouAEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  return_array = []
+  input_array = text.split(" ")
+  input_array.each do |word| 
+    return_array << word if word.match(/\bun\S*ing\b/)
+  end
+  return_array
 end
 
 def words_five_letters_long(text)
-
+  five_letter_words = []
+  input_array = text.split(" ")
+  five_letter_words = input_array.grep(/\b[a-zA-Z]{5}\b/)
+  five_letter_words
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  !!text.match(/^[A-Z].*[!.?]$/)
 end
 
 def valid_phone_number?(phone)
-
+  # binding.pry
+  !!phone.match(/.{0,1}[0-9]{3}.{0,1}[0-9]{3}.{0,1}[0-9]{4}$/)
 end
