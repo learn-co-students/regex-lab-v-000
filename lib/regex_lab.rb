ea7ac22640026e7d66d5c4e34d1cf3c22684ea9d
jsgoldb <jgoldb@yahoo.com>
2015-12-21 17:06:12 -0600

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,34 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+  if word.match(/^[aeiouAEIOU]/) != nil
+    return true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+  text.scan(/un\S*ing/)
 
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z].*\W$/) != nil
+    return true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/^(\D?\d){10}$/)
+    return true
+  else
+    false
+  end
 end
