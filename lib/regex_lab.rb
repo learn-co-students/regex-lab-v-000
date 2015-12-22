34a73c8d008457fc1bac8a63b79f344c27609a52
ryuichi7 <brettrheenan@gmail.com>
2015-12-22 11:54:14 +0100

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,21 @@
+require 'pry'
 def starts_with_a_vowel?(word)
-
+  word.match(/\b[aeiouAEIOU]/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b[a-zA-Z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
+  text.match(/\b[A-Z]\W\b/) ? true : false
 
 end
 
 def valid_phone_number?(phone)
-
+  phone.scan(/[\d]/).count == 10 ? true : false
 end
