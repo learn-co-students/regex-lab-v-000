88b7d0099eba00017b1b9a47a7b43f82a7f7df42
John Anthony <askinganthony@gmail.com>
2015-12-31 05:49:14 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,20 @@
 def starts_with_a_vowel?(word)
-
+  word.match(/\A[aeiouAEIOU]/)!= nil ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/(un[a-zA-Z]*ing)/).flatten
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b([a-zA-Z]{5})\b/).flatten
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/^[A-Z][\W\w]*[.!?]\z/) != nil ? true : false
 end
 
 def valid_phone_number?(phone)
-
+  phone.gsub!(/\D/, '')
+  phone.length == 10 ? true : false
 end
