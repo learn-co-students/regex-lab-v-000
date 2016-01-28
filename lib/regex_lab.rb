00e8289012c49a3f8e2b8290c7534a26b453583a
reifnotreef <bridgerrhammond@gmail.com>
2016-01-28 11:16:31 -0600

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,22 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/^[aeiouAEIOU]\w+/)
+    true
+  else false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un+\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/^[A-Z].+\r?[\.!?]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
-
+  phone.scan(/.\d{2}.\d{3}.\d{2,4}\b/).count > 0 ? true : false
 end
