1e083dee70bcb5303b1e47dfe04a07ec52e82ac7
NStephenson <nickstephenson90@gmail.com>
2016-02-02 16:13:44 -0500

    made all tests pass

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,33 @@
-def starts_with_a_vowel?(word)
 
+def starts_with_a_vowel?(word)
+  if word.match(/\b[AEIOUaeiou]\w+/)
+    true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+  text.scan(/un\w+ing/)
 
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/\A[A-Z]+\W\b/)
+    true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/\W?\d{3}\W?\d{3}\W?\d{4}\b/)
+    true
+  else
+    false
+  end
 end
