a6a79b5d34a701e15e4e02284bd5b902b2e18ba1
evanscloud <dijkstrascloud@gmail.com>
2016-02-03 09:54:35 -0500

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,36 @@
-def starts_with_a_vowel?(word)
+# scan: returns all items that match string
+# match: returns first item that match string
+#GREP: searches arrays and hashes
+require 'pry'
 
+def starts_with_a_vowel?(word)
+  if word.match(/^[AEIOUaeiou]\S+/)
+    true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z].+[.?!]$/)
+    true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/^(\+\d{1}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/)
+    true
+  else
+    false
+  end
 end
