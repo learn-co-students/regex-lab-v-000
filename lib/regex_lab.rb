c22c46af69d3174e84203fedf9ca35be9e8c800e
jjlippman <jjlippman@earthlink.net>
2016-02-05 17:17:35 -0700

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,37 @@
 def starts_with_a_vowel?(word)
-
+   if word.match(/^[aeiouAEIOU]/)
+    true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+   text.scan(/\bun\w+ing/)
+    
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
+  if text.match(/\A[A-Z].+[?.!]\Z/)
+    true
+  else
+    false
+  end
 
 end
 
 def valid_phone_number?(phone)
-
+ arr=phone.scan(/\d/)
+ if arr.size == 10
+  true
+ else
+  false
+ end
 end
+
+
+
