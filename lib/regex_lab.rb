bf4085da939f748ce6e3bf1926e8c9fa339c799d
jbok4 <jbok4@aol.com>
2016-02-21 18:59:30 -0500

    fixed phone validator

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -35,7 +35,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match(/1?\W*([2-9][0-8][0-9])\W*([2-9][0-9]{2})\W*([0-9]{4})(\se?x?t?(\d*))?/)
+  if phone.match(/1?\W*([2-9][0-8][0-9])\W*([2-9][0-9]{2})\W*([0-9]{4})?/)
     true
   else
     false
