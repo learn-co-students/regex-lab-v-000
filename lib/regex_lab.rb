0f05644e47ab7a992c2cab1c5e88e00cad612b92
mcclains2003 <mcclains2003@gmail.com>
2016-02-29 13:32:28 -0600

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-
+  !!word.match(/\A[aeiou]/i)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un+\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  !!text.match(/[A-Z]+\w+[,.?!]/)
 end
 
 def valid_phone_number?(phone)
-
+  !!phone.match(/\W*([2-9][0-8][0-9])\W*([2-9][0-9]{2})\W*([0-9]{4})/)
 end
