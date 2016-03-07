2ad64a4fded851001f1979a8ceafe4ba925eae5c
Rami97 <rami-sebastian@web.de>
2016-03-07 20:41:17 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,28 @@
 def starts_with_a_vowel?(word)
-
+if word.match(/\A[AEIOUaeiou]/)
+  return true
+  else
+  return false
+end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/\bun\S*ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b[a-zA-Z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+if text.match(/\A[A-Z].*[:|,|.|!|?]\z/)
+  return true
+  else
+  return false
+end
 end
 
 def valid_phone_number?(phone)
-
+  new_number = phone.scan(/\d/)
+  new_number.join("").match(/\d{10}/) ? true : false
 end
