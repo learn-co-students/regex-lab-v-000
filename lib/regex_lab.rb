d12127ad59cb257aad947507178a1490f4c0e0d9
Robert Koeze <rkoeze@gmail.com>
2016-03-13 18:50:47 -0700

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,17 @@
 def starts_with_a_vowel?(word)
-  word.match(/\A[aeiouAEIOU]/) ? true : false
+  word.match(/\b[aeiouAEIOU]/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/un\w+ing/)
+  text.scan(/\b[un]\S*[ing]/)
 end
 
 def words_five_letters_long(text)
-  text.scan(/\b[a-zA-Z]{5}\b/)
+  text.scan(/\b[A-z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/\A[A-Z].*\W\z/) ? true : false
+  text.match(/\A[A-Z].*[.!]/) ? true : false
 end
 
 def valid_phone_number?(phone)
