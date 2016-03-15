a3e0480342809307bacf3f7f206d55c98b7388de
RMcNeely <rmcneely23@gmail.com>
2016-03-15 16:41:18 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,27 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\b[aeiouAEIUO]/)
+    true
+   else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+text.scan(/\b+un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b[a-zA-Z]{5}\b/).to_a
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/\b[A-Z]\p{P}/)
+    true
+    else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
-
+  true if phone.match(/\W*([2-9][0-8][0-9])\W*([2-9][0-9]{2})\W*([0-9]{4})?/)
 end
