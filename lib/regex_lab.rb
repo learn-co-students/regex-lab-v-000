73a6c26e65010f613e0aa400cc99f272dfc74570
jchu4483 <jchu4483@gmail.com>
2016-03-15 20:35:53 -0400

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -13,8 +13,10 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/\b[A-Z].+[.!?]\b/)
+  text.match(/^[A-Z].+[\.!?]$/) ? true : false
 end
+
+
 
 def valid_phone_number?(phone)
 
