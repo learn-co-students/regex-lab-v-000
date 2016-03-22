fae000f7a0c60af3a593c8b997216f179078c754
viparthasarathy <viparthasarathy@gmail.com>
2016-03-21 18:08:36 -0700

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def starts_with_a_vowel?(word)
-  word.match(/^[aeiouAEIOU]/).to_s != ""
+  word.match(/^[aeiouAEIOU]/) != nil
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
