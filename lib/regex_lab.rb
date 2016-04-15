8b2552e38c92ec71cfc0520ed63af49afd27ca6f
kaileeagray <kaileeagray@gmail.com>
2016-04-15 12:03:06 -0500

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,18 +1,19 @@
 def starts_with_a_vowel?(word)
-  word.match(/\A[aeiou]/) == nil ?
+  word.match(/^[aeiouAEIOU]/) == nil ?
     false : true
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\w*ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.scan(/\A[A-Z].*[^\w\s]$/).count > 0 ?
+  true : false
 end
 
 def valid_phone_number?(phone)
