4c01a4b586b897a1128bcd9a8465b40ecd056be2
MIKE-RANDELL <michael.james.randell@gmail.com>
2016-04-17 16:28:52 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,15 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\b[aeiouAEIOU]/)
+    return true
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  def count
+    
+  end
 end
 
 def words_five_letters_long(text)
