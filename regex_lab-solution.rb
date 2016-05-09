d5540b00d27705ad70255cd4b8e1e5557376a0d6
Being Young <beingy@gmail.com>
2016-05-09 16:00:21 -0400

    Update solution with Rubular permalink

--- regex_lab-solution.rb
+++ regex_lab-solution.rb
@@ -15,7 +15,7 @@ end
 def first_word_capitalized_and_ends_with_punctuation?(text)
   text.match(/^[A-Z].+[\.!?]$/) ? true : false
 end
-http://rubular.com/r/oBfG4I0bac
+# http://rubular.com/r/oBfG4I0bac
 
 def valid_phone_number?(phone)
   phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) ? true : false
