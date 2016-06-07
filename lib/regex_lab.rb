8f32443ed6e623802ff8830dcecd123c124960c1
Lenn Hypolite <lenntriniman@hotmail.com>
2016-06-07 02:25:04 +0000

    refactor code by replacing the if else statements with the ternary operator

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,9 @@
 def starts_with_a_vowel?(word)
-  if word.match(/\A[aeiou]/i) #check all words to see if they start with a vowel
-    true
-  else
-    false
-  end
+  word.match(/\A[aeiou]/i) ? true : false #check all words to see if they start with a vowel
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-    return text.scan(/\w+ing/)
+  return text.scan(/\w+ing/)
 end
 
 def words_five_letters_long(text)
@@ -15,17 +11,9 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/^[A-Z].*[\-,.!?]$/)
-    true
-  else
-    false
-  end
+  text.match(/^[A-Z].*[\-,.!?]$/) ? true : false
 end
 
 def valid_phone_number?(valid_numbers)
-  if valid_numbers.match(/\(?\d{3}\)?[- ]?\d{3}[- ]?\d{4}/)
-    true
-  else
-    false
-  end
+  valid_numbers.match(/\(?\d{3}\)?[- ]?\d{3}[- ]?\d{4}/) ? true : false
 end
