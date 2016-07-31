420252b04e297b346ea33df11fd8a1ce2fcf4462
jinstrider2000 <efrainperezjr@live.com>
2016-07-31 00:03:38 -0400

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-
+  /\A[aeiouAEIOU]\w*/ =~ word ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\w*ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text =~ /\A[A-Z][\w\W]*[.?!]\Z/ ? true : false
 end
 
 def valid_phone_number?(phone)
-
+  phone =~ /\A\({,1}[0-9]{3}[\)\- ]{,1}[0-9]{3}[\- ]{,1}[0-9]{4}\Z/ ? true : false
 end
