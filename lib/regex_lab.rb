cc6cbf13806077872ca37e07f5a60cc226fbc3ac
Baruch Steinmetz <boruchsteinmetz@outlook.com>
2016-08-01 00:16:40 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,30 @@
 def starts_with_a_vowel?(word)
+   if word.match(/^[aeiouAEIOU]/)
+     true
+   else
+     false
+   end
+
 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+  text.scan(/un.*?ing/)
+
 
 end
-
 def words_five_letters_long(text)
+  text.scan(/\b\w{5}\b/)
+
 
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z].*\./)
+     true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
