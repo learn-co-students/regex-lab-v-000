8d4301d4e51ccedd00b78ea5e074f4b14f601da8
Christina Cole <christinal.cole@gmail.com>
2016-08-02 18:34:48 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -24,10 +24,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text) # start here
 end
 
 def valid_phone_number?(phone)
-  #if phone.match(/\d{10} | /)
-  #if phone.scan(/[0-9]{10}|\(\d{3}\)\d{3,}.\d{3,4}|\d{3,4}/)
-  #if phone.match(/^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/)
-  if phone.match(/[(]?\d{3}.? ?\d{3}.?\d{4}\b/)  
+    if phone.match(/[(]?\d{3}.? ?\d{3}.?\d{4}\b/)  
     true
   else
     false
