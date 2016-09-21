357c31d63d6213dfb0fa6030c1f463d7d1c3ef29
William Story <wstory@villanova.edu>
2016-09-21 05:07:27 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,37 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\A[aeiouAEIOU]/)
+    true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun[a-z]+ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b[a-z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/\A[A-Z].+[.?!]\z/)
+    true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/\d{10}/)
+    true
+  elsif phone.match(/\W\d{3}\W\d{3}\W\d{4}/)
+    true
+  elsif phone.match(/\d{3}\s\d{3}\s\d{4}/)
+    true
+  elsif phone.match(/\W\d{3}\W\d{7}/)
+    true
+  else
+    false
+  end
 end
