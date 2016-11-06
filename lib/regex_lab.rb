ed49a9cb4e031c257336b9fa5cc3d3c638cd0e60
Elvin <elvin@elvingarcia.com>
2016-11-05 21:56:26 -0400

    refactored valid_phone_number?

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -18,7 +18,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match(/"?\(?\d\d\d\)?\s?\-?\d\d\d\-?\s?\d\d\d\d"?/)
+  if phone.match(/"?\(?\d{3}\)?\s?\-?\d{3}-?\s?\d{4}"?/)
     return true
  else
   return false
