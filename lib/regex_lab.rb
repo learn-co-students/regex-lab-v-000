1160023af0c98546f5c13838e1a4f9a6157773fd
Frederico  Morato <freddymorato@gmail.com>
2016-12-28 20:57:41 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,20 @@
-def starts_with_a_vowel?(word)
 
+def starts_with_a_vowel?(word)
+  word =~ /^[aeiou].+/i ? true : false # could use .* or \w* in place of .+
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un+\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text =~ /^[A-Z].+[[:punct:]]$/ ? true : false # could use .* in place of .+
 end
 
 def valid_phone_number?(phone)
-
+  phone =~ /^\D?\d{3}\D?\d{3}\D?\d{4}$/ ? true : false
 end
