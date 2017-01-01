42418e33c788b149a85cb6c4ffdcc9aa4b204a48
Tom Su <djtomblue@gmail.com>
2017-01-01 23:05:42 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,42 @@
-def starts_with_a_vowel?(word)
+require 'pry'
+# match = %w{ afoot Excellent incredible Absolute unreal Inconceivable octopus }
 
+def starts_with_a_vowel?(word)
+  if word.match(/^[aeiouAEIOU]/) != nil
+    true
+  else
+    false
+  end
 end
 
+#incomplete
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  array = []
+  array = text.scan(/\b\w{5}\b/)
 end
+
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z]/) != nil && text.match(/[.?]$/) != nil
+    true
+  elsif text.match(/^[A-Z]/) == nil && text.match(/[.?]$/) != nil
+    false
+  elsif text.match(/^[A-Z]/) != nil && text.match(/[.?]$/) == nil
+    false
+  elsif text.match(/^[A-Z]/) == nil && text.match(/[.?]$/) == nil
+    false
+  end
 end
 
+#incorrect
 def valid_phone_number?(phone)
-
+  if phone.match(/[A-Z]/) == nil
+    true
+  else
+    false
+  end
 end
