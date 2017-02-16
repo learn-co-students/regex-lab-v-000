32005f0addb103f8417e889090164d1f9c574396
Dalma Boros <dalma.boros@gmail.com>
2017-02-16 15:56:49 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,21 @@
 def starts_with_a_vowel?(word)
-
+  return true if word.match(/^[aeiouAEIOU]/)
+  else return false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\w*ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  return true if text.match(/^[A-Z].*\W$/)
+  else return false
 end
 
 def valid_phone_number?(phone)
-
+  return true if phone.match(/\d{3}.*\d{3}.*\d{4}/)
 end
