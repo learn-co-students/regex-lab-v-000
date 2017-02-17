c84c7a5d8b357b1139bf2e428bad5db7c66b65e3
Kelly Kashuda <klkashuda@crimson.ua.edu>
2017-02-17 03:46:27 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,31 @@
 def starts_with_a_vowel?(word)
+  puts word
+  result = word.match(/^[aAeEiIoOuU]/)
+  puts "RESULT"
+  puts result
+  if result == nil
+    false
+  else
+    true
+  end
 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/(un\w+)/).flatten
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\s([a-z]{5})\b/).flatten
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/(^[A-Z]).*[.]/) == nil
+    return false
+  end
+  return true
 end
 
 def valid_phone_number?(phone)
-
+  phone.match(/\d[0-9]\)*\z/)
 end
