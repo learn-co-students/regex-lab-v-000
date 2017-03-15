9a37a8450d3bf38f7bf9f0611c4896f98ee21cf2
thesilentc <thesilentc@me.com>
2017-03-15 09:44:02 -0400

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,9 +11,9 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  !text.match(/^[A-Z]{1}[\w\W]+[,.?!]$/).nil?
+  text.match(/[A-Z]\w+[[:punct:]]\s/) ? true : false
 end
 
 def valid_phone_number?(phone)
-  !phone.match(/\({1}?[\d]{3}[-)\s]{0,3}[\d]{3}[\s-]{0,2}\d{4}/).nil?
+  phone.match(/\d{3}.+\d{3}.\d{4}/) || phone.match(/\d{7}/) ? true : false
 end
