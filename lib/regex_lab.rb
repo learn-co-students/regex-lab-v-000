1e39990e986d04898b2c219ca2b6a61dbfa61171
Siobhan Martin <bklyngal1994@gmail.com>
2017-04-05 03:31:56 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,30 @@
 def starts_with_a_vowel?(word)
+  if word.match /^[AEIOUaeiou]/
+    true
+  else
+    false
 
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+  text.scan(/\bun\w+ing\b/)
 
 end
 
 def words_five_letters_long(text)
-
+text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
+  if text.match(/^[A-Z].+[\.!?]$/)
+    true
+  else
+    false
 
+  end
 end
 
 def valid_phone_number?(phone)
-
+phone.match(/[\d]{3}\D*[\d]{3}\D*[\d]{4}/) ? true : false
 end
