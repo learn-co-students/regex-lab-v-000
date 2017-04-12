a7b9c2ff78893809e027d913a5bf4f42a05ceec9
David Mars <david.w.mars@my.occc.edu>
2017-04-12 02:52:51 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,22 @@
 def starts_with_a_vowel?(word)
-
+ word.match(/\b[aeiouAEIOU][a-z]*\b/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
 
-end
+text.scan(/\bun[a-z]*\ing\b/)
+
+  end
+
 
 def words_five_letters_long(text)
-
+text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/\b[A-Z][a-z]*[!,.;?]/) ? true : false
 end
 
 def valid_phone_number?(phone)
-
+  phone.gsub(/[-\(\)" "]/, "").match(/\d{10}/) ? true : false
 end
