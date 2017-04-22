ae25735653dcacf9f8648e5fe22dc017cf4837a3
Heather You <heatherhyou@gmail.com>
2017-04-22 20:31:09 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,11 +7,11 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/^un.*ing$/)
+  text.scan(/un\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-  text.scan(/\w{5}\S/)
+  text.scan(/\^[a-z]{5}$/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
@@ -19,5 +19,9 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-
+  phone.each do |digits|
+    if digits.match(/\d{10}/)
+      true
+    end
+  end
 end
