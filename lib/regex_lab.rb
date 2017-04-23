36fe8c5ce49359b924192ea414cd102b87848552
Heather You <heatherhyou@gmail.com>
2017-04-23 02:27:24 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,17 +11,19 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  text.scan(/\^[a-z]{5}$/)
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z].+[\.!?]$/)
+    true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
-  phone.each do |digits|
-    if digits.match(/\d{10}/)
-      true
-    end
+  if phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/)
+    true
   end
 end
