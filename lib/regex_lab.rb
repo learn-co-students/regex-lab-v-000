5243105c1696581ec7f49d332915a885addb96f0
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2017-04-26 18:14:24 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,15 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+  if word.match(/\A[^aeiouAEIOU]/)
+     false
+  else
+    true
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\Aun\Zing/)
 end
 
 def words_five_letters_long(text)
