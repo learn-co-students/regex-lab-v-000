7fd2f004d56c9760a0eefb0a24c691e9b909b0aa
Joseph Kim <josephkim0224@gmail.com>
2017-05-26 16:13:14 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-
+  !!word.match(/^[aeiou]/i)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+ing/i)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  !!text.match(/^[A-Z].+\.{1}$/)
 end
 
 def valid_phone_number?(phone)
-
+  !!phone.match(/\({0,1}\d{3}(\)|\)\s|\s){0,1}\d{3}(-|\s){0,1}\d{4}\b/)
 end
