29bcabc99e683e2aefe1dc03b78fb6fe76229a29
Luisa Scavo <luisascavo@gmail.com>
2017-05-29 14:09:45 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,20 +3,17 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  uning_words =text.split.map do |word|
-    word.scan(/^un\w+ing/)
-  end
-  uning_words.flatten
+  text.scan(/\bun\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-  
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  !(text.match(/^[A-Z].+[.,?!]$/).nil?)
 end
 
 def valid_phone_number?(phone)
-
+  !(phone.match(/\({0,1}\d{3}[-) \.]{0,1}\d{3}[- \.]{0,1}\d{4}/).nil?)
 end
