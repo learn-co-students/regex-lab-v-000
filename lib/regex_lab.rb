42f263bd7754631dd30ec70499d1ae8d23e24359
Luisa Scavo <luisascavo@gmail.com>
2017-05-29 01:24:03 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,16 @@
 def starts_with_a_vowel?(word)
-
+  !(word.match(/^[aeiouAEIOU]/).nil?)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  uning_words =text.split.map do |word|
+    word.scan(/^un\w+ing/)
+  end
+  uning_words.flatten
 end
 
 def words_five_letters_long(text)
-
+  
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
