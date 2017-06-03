f3551248b4a8355f3ceb7cd7644c4e6d717f5064
Nicholas D'Amico <nickalan82@icloud.com>
2017-06-03 13:07:55 +0000

    refactor

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,11 +1,12 @@
 require "pry"
 
 def starts_with_a_vowel?(word)
-  word.match(/^[aeiou]|[AEIOU]/) ? true : false
+  word.match(/^[aeiou]|[AEIOU]\w+/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/un\w+ing/)
+  # Unsure of why /^un\w+ing$/ wouldn't work??
+  text.scan(/un\w+ing\b/)
 end
 
 def words_five_letters_long(text)
@@ -13,9 +14,11 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/[A-Z]\w+[.,?!]/) ? true : false
+  text.match(/[A-Z]\w+[\.,!?]/) ? true : false
 end
 
 def valid_phone_number?(phone)
   phone.scan(/\d+[^\W]/).join.length == 10 ? true : false
+
+  # phone.match(/([0-9] *?){10}|(\(*[0-9]{3}\)*([0-9]{3}-[0-9]{4}|[0-9]{7}\b))/)
 end
