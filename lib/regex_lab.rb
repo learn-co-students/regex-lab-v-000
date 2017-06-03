ddb88c9546ca1a96876b102782f6c94c3114d516
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-06-03 19:17:57 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,33 @@
+# returns T for words starting with a vowel and F for words starting with consonant
 def starts_with_a_vowel?(word)
-
+   if word.match(/\b[AEIOUaeiou]/)
+     true
+   else
+     false
+   end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\b[Uu]n[a-z]*ing\b/)
 end
 
 def words_five_letters_long(text)
+  text.scan(/\b\w{5}\b/)
 
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z].*\W$/)
+    true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/(\d)\w(\d)(\d)/)
+    true
+  else
+    false
+  end
 end
