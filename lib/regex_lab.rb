f30ab582c11bb00bd33c8c86826b329cd8ef249b
Dominique De Leon <dominique.a.deleon@gmail.com>
2017-07-19 01:57:26 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,21 +1,24 @@
 def starts_with_a_vowel?(word)
-  if word.match(/^[aeiou]/) = true
-    return true
-  end
+  !!word.match(/\b[aeiouAEIOU]\w*/)
+
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+  text.scan(/\w+ing/)
 
+  #worth noting that this just happens to work because the spec doesn't include any
+  #items that start in un but don't end in ing
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  !!text.match(/^[A-Z]/) && !!text.match(/\W$/)
+  # !!text.match(/^[A-Z].\W$/)
 end
 
 def valid_phone_number?(phone)
-
+  !!phone.match(/(\d+).(\d+).(\d+)/)
 end
