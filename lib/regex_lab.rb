b3772706f056bda4f8d6ffc5f8422dc9d1587c9b
kylek <kbelway29@gmail.com>
2017-08-16 11:51:55 -0400

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,22 @@
 def starts_with_a_vowel?(word)
-
+  /\A[aeiouAEIOU]/ === word
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  array = text.split()
+  starts_un = array.grep(/^\w[un]/)
+  starts_un.grep(/(ing)\z/)
 end
 
 def words_five_letters_long(text)
-
+  array = text.split()
+  array.grep(/\A\w{5}\z/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.lines.grep(/\A[A-Z]/).join().match(/\W\z/) != nil
 end
 
 def valid_phone_number?(phone)
-
+  phone.match(/\d{3}\D*\d{3}\D*\d{4}\z/) != nil
 end
