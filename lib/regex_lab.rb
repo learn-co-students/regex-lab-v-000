eedddccd7169042db852b87134c4719838032ea8
Archana Burra <burra.archana@gmail.com>
2017-08-16 14:09:40 -0400

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,13 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/^[aeiouAEIOU]\w*/) != nil
+    return true
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\A[un]\w*[ing]\z/)
 end
 
 def words_five_letters_long(text)
