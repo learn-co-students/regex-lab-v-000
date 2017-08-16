c12fe53667263ec0e065a25b40281375628570ef
suzanne baldwin <gbaldwin24@comcast.net>
2017-08-16 19:00:26 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,34 @@
 def starts_with_a_vowel?(word)
+  if word.match(/^[aeiouAEIOU]\w/)
+true
+  else
+false
 
+
+
+  end
 end
-
 def words_starting_with_un_and_ending_with_ing(text)
-
+text.scan(/\bun\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-
+text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z].*\W$/)
+    true
+  else
+    false
+end
 end
 
 def valid_phone_number?(phone)
+  if  phone.match(/[0-9]{3}[0-9]{3}[0-9]{4}|\D[0-9]{3}\D[0-9]{3}-[0-9]{4}|[0-9]{3}\s[0-9]{3}\s[0-9]{4}|\D[0-9]{3}\D[0-9]{7}/)
+    true
+  else
+    false
+end
 
 end
