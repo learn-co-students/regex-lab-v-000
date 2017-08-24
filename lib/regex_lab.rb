d98aeb43e4b4e7210d2ad09427e36187ab2f6629
Jameson Bass <jamesonbass@gmail.com>
2017-08-24 22:46:24 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,23 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+    word.to_s.match(/^[aeiouAEIOU]/)? true : false
 end
 
-def words_starting_with_un_and_ending_with_ing(text)
 
+def words_starting_with_un_and_ending_with_ing(text)
+    text.scan(/un\w+\ing/)
 end
 
 def words_five_letters_long(text)
-
+    text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    text.match(/^[A-Z].*[.?!]$/)? true : false
 end
 
 def valid_phone_number?(phone)
-
+    phone_array = phone.scan(/^(\(?)(\d{3})[\)\s{1,2}]?(\d{3})[-\)\s]?(\d{4})$/)
+    phone_array.length > 0? true : false
 end
