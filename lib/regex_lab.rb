3a1110700efa7dd25a5369f02bc7411fec163a89
Justin Tooley <jstooley823@gmail.com>
2017-09-09 02:48:56 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,25 @@
 def starts_with_a_vowel?(word)
 
+  !!word.match(/^[aeiou]/i)
+
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.split.grep(/^(un)|(ing)$/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
+
+  !!text.match(/^[A-Z].*\W$/)
 
 end
 
 def valid_phone_number?(phone)
 
+  !!phone.match(/^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/)
+    
 end
