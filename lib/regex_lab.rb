82a71b5be601e7b2b8d6fce17a1e7cf99253940f
smithwebtek <brad@smithwebtek.com>
2017-09-12 15:45:59 -0400

    refactor upon restudying

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,11 @@
 require 'pry'
 
 def starts_with_a_vowel?(word)
-  if word.match(/\b[aeiou]/i) 
-    return true
-  else false
+	rxp = Regexp.new(/\A[aeiou]{1}/i)
+  if word.match(rxp) 
+    true
+	else 
+		false
   end
 end
 
@@ -12,13 +14,15 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  text.scan(/\b\w{5}\b/)
+  text.scan(/\b[\w]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/^\A[A-Z][\w \d(\W )]+\W\z/)
-    return true
-  else false
+  # if text.match(/^\A[A-Z][\w \d(\W )]+\W\z/)
+  if text.match(/(\A[A-Z][\w]*)\s.*(\W\z)/)
+    true
+	else 
+		false
   end
 end
 
