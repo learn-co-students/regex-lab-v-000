e49781df969666b77b8c374a0d08768d33c95e78
Kevin Huang <yellow.khaos@gmail.com>
2017-10-21 17:00:18 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/\bun\w*ing/)
+  text.scan(/un\w*ing\b/)
 end
 
 def words_five_letters_long(text)
@@ -15,17 +15,9 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/\A[A-Z].*[.!?]\z/)
-    return true
-  else
-    return false
-  end
+  text.match(/\A[A-Z].*[.!?]\z/) ? true : false
 end
 
 def valid_phone_number?(phone)
-  if phone.match(/[(]?([0-9]{3})[- )]?([0-9]{3})[- ]?([0-9]{4})/)
-    return true
-  else
-    return false
-  end
+  phone.match(/[(]?([0-9]{3})[- )]?([0-9]{3})[- ]?([0-9]{4})/) ? true : false
 end
