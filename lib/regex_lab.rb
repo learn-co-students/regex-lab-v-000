4d1504d854cd7bc30f724ccb6688c99eae56dd91
Rebecca Tong <rtong@buffalo.edu>
2017-10-27 03:29:44 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,29 @@
+'require pry'
 def starts_with_a_vowel?(word)
-
+  if word[0].match(/[aeiou]/i)
+    return true
+end
+return false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
-end
+  text.scan(/\b[a-zA-Z]{5}\b/)
+  end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z].+[\.!?]$/)
+    return true
+  end
+  return false
 end
 
 def valid_phone_number?(phone)
-
+if phone.match(/\d{10}|\d{3}\W\d{3}\W\d{4}|\(\d{3}\)\d{7}/)
+  return true
+end
+return false
 end
