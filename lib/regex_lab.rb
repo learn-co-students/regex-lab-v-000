965cfed792e678898c2657bb60ab0181b07415d7
Amanda Ackerman <ackerm44@gmail.com>
2017-10-29 15:05:54 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,34 @@
 def starts_with_a_vowel?(word)
+   if (/\b[aeiouAEIOU]\w*/) === word
+     return true
+   else
+     return false
+   end
 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\w*/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if (/\A[A-Z]/) === text && (/\./) === text
+    true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
-
+  length = 0
+  length = phone.scan(/\d/).size
+  if length != 10
+    false
+  else
+    true
+  end
 end
