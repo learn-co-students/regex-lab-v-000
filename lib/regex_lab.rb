5f6b1d0acb7a128e823701ab01d0e9a07e18f10b
charityyoungblood <youngblood.charity@gmail.com>
2017-11-03 15:36:57 -0700

    changed logic for starts_with_a_vowel? method

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,10 @@
 def starts_with_a_vowel?(word)
-
+ 
+   word.scan(/[aeiou].*/)
+   if # if word.scan returns a value, the expression is true 
+    
+   end
+  
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
