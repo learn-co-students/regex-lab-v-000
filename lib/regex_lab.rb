01fbb7309082260f72063f3b9c889b19c247f1f1
Chad Montoya <chadmontoya21@yahoo.com>
2017-11-07 04:56:57 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,23 @@
+require 'pry'
+
 def starts_with_a_vowel?(word)
+  word.match(/\b[aeiouAEIOU]/) ? true : false
 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\w+[un]+[ing]/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
+  text.match(/^[A-Z].+[.!?]$/) ? true : false
 
 end
 
 def valid_phone_number?(phone)
-
+  phone.match(/\D*([2-9]\d{2})(\D*)([2-9]\d{2})(\D*)(\d{4})\D*/) ? true : false
 end
