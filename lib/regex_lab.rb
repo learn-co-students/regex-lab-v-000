e143b3050635db2a40b925b57c3f7e85ebf39262
Elle Faraday <faraday.elle@gmail.com>
2017-11-07 03:22:42 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,37 @@
-def starts_with_a_vowel?(word)
 
+
+def starts_with_a_vowel?(word)
+  if word.match(/\b[aeiouAEIOU]\w*/) == nil
+    false
+  else
+    true
+  end
 end
 
+puts starts_with_a_vowel?("octopus")
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\w*ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[a-z]\w*\s*\w*\s*\w*\s*\w*\s*\w*\s*\w*\s*\w*\s*\w*['!"#$%&\\'()\*+,\-\.\/:;<=>?@\[\\\]\^_`{|}~']/) != nil
+    false
+  elsif text.match(/[a-z]$/)
+    false
+  else text.match(/\b[A-Z]\w*['!"#$%&\\'()\*+,\-\.\/:;<=>?@\[\\\]\^_`{|}~']/) != nil
+    true
+  end
 end
 
 def valid_phone_number?(phone)
-
+  phone.delete! '() -'
+  if phone.match(/\b\d{10}\b/) != nil
+    true
+  else
+    false
+  end
 end
