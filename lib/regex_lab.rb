aae7b19578c11b3caf6d35574afcd4d678907f2a
Charity <Youngblood.charity@gmail.com>
2017-11-07 18:37:35 -0800

    added new regex to test; successful

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,10 +11,8 @@ end
 
 def words_starting_with_un_and_ending_with_ing(text)
   # match the "un" and "ing" then return the matching words
-  find_text = text.scan(/\w+[un][ing]/)
-    
+  find_text = text.scan(/\w+[un][ing]/) 
  
-  binding.pry
   
 end
 
