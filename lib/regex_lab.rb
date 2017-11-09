4ec640d4b39802033eb77860d89d1b3d14183ffa
Charity <Youngblood.charity@gmail.com>
2017-11-08 16:08:13 -0800

    testing regex for invalid phone numbers

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -28,6 +28,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  valid = phone.match(/\d{10}|[^\W]/)
+  valid = phone.scan(/\d{10}|[^\Wa-zA-Z]/)
+ 
   
 end
