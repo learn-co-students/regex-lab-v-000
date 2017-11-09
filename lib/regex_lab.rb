0e39a5c119226f3ad377d5afd7af801f61577dd9
charityyoungblood <youngblood.charity@gmail.com>
2017-11-09 09:18:34 -0800

    test

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,11 +17,11 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-
+    five_words = text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    
 end
 
 def valid_phone_number?(phone)
