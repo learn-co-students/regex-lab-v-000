983f0c41d31d2eac94f07ad4ad6e9b40d6e8d1f0
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-29 17:57:28 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,9 @@
 def starts_with_a_vowel?(word)
-  /\A[aeiou]/i.match(word) != nil
+  /\b[aeiou]/i.match(word) != nil
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/un\w*ing/i)
+  text.scan(/\bun[a-z]*ing\b/i) #return
 end
 
 def words_five_letters_long(text)
@@ -11,7 +11,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  /\b[A-Z][a-z]*[^\w\s]\s/.match(text) != nil
 end
 
 def valid_phone_number?(phone)
