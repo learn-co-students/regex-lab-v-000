6bcc91aba55f7b23048f3f39bbf612049a8a211c
github username <github email address>
2017-12-06 08:32:24 -0500

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,30 @@
 def starts_with_a_vowel?(word)
-
+#returns true for words starting with a vowel
+#returns false for words starting with a consonant
+#if not using .capitalize, only one test passes.. Why?
+  !!word.capitalize.match(/\A+[AEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\b[Uu]n[a-z]*ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z].+[\.!?]$/)
+   true
+    else
+   false
+    end
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/)
+   true
+  else
+   false
+    end
 end
