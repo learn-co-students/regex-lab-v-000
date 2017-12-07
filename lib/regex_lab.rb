1286721b7211c882e2d6e5e6ad1e3b0640c0013e
Jonathan Luk <jluk87@gmail.com>
2017-12-07 23:18:41 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,22 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+  word.scan(/^[aeiouAEIOU]/).length > 0? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b[a-zA-Z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.scan(/^[A-Z].+[.]/).length > 0? true : false
 end
 
 def valid_phone_number?(phone)
+  phone.scan(/[0-9a-zA-Z]/).length == 7 || phone.scan(/[0-9a-zA-Z]/).length == 10? true : false
 
 end
