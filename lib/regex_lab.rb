ee1d2b55117bf210db6482e14475d4b2ea38b582
Joey Cho <cjy12182000@naver.com>
2017-12-12 12:53:45 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,31 @@
 def starts_with_a_vowel?(word)
-
+    if(word.match(/^[AEIOUaeiou]/)!= nil)
+      return true
+    else
+      return false
+    end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  return text.scan(/un\w*ing/)
 end
 
 def words_five_letters_long(text)
-
+  return text.scan(/(?<=^|\s)[A-Za-z]{5}(?=\s|$)/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if(text.match(/[A-Z]+\W/)!=nil)
+    return true
+  else
+    return false
+  end
 end
 
 def valid_phone_number?(phone)
-
+  if(phone.match(/\(?\d{3}[\)\s]?\d{3}[\-\s]?\d{4}/)!=nil)
+    return true
+  else
+    return false
+  end
 end
