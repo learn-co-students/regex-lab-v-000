e9e3a72d05eb5e083e931c03d896f6f40699e121
Jon Hinson <jonhinsonfx@gmail.com>
2017-12-14 00:26:18 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,12 +1,21 @@
+require 'pry'
 def starts_with_a_vowel?(word)
-  /[aeiou]/
+  if word.match(/\A[aAeEiIoOuU]/)
+    true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+ new_array = text.scan(/\w+ing/)
+ new_array
 end
 
 def words_five_letters_long(text)
+  new_array = text.grep(/^\w{5}\s/)
+  binding.pry
+  new_array
 
 end
 
