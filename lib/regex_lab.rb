1eff5eb6064e7c571db71f9212287a585bf3da94
Jon Hinson <jonhinsonfx@gmail.com>
2017-12-14 04:36:33 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,14 +14,15 @@ end
 
 def words_five_letters_long(text)
   new_array = text.scan(/\b\S{5}\b/)
-
   new_array
-
-
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/\A[aAeEiIoOuU]/)
+    true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
