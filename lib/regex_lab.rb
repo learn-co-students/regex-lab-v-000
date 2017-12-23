035f821f1bb7e1a0a940b4d2a175aa13870181c5
jtorre20 <jtorres1989@gmail.com>
2017-12-23 14:31:31 -0500

    finished all test

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,10 +1,6 @@
 require 'pry'
 def starts_with_a_vowel?(word)
-   if word.match(/\b[aeiouAEIOU]/) 
-    true
-   else 
-    false
-   end
+  word.match(/\b[aeiouAEIOU]/) ? true : false 
     
 end
 
@@ -13,13 +9,15 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/\b[A-Z]+\W\b/) ? true : false
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/.*\d{3}.?\d{3}.?\d{4}.*/) 
+    true
+end
 end
