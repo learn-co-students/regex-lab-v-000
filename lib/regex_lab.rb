e26c1178d3d9c209f4e60bc812b4a462e149be00
Aubrey Neimeier <neimeier.1@osu.edu>
2018-01-05 16:41:08 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,15 @@
+##to specifcy a certain character (not a metacharacter),
+#like if you are looking for a parenthesis, you have to 'escape' first.
+# aka use a backslash.
+
+#for example "may have a (" would be "\(*"
+
 def starts_with_a_vowel?(word)
   word.match(/\b[AEIOUaeiou][a-z]*\b/)? (true) : (false)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/\bun[a-z]*ing[a-z]*/)
+  text.scan(/un\w+ing\b/)
 end
 
 def words_five_letters_long(text)
@@ -11,9 +17,13 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/\b[A-Z][a-z]*,/)? (true) : (false)
+  text.match(/^[A-Z].+[\.!?]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
   phone.match(/\(*\d{3}\D*\d{3}\D*\d{4}\b/)? (true) : false
 end
+
+#avi's solution groups by number
+# (10 numbers) OR (three numbers) with (three numbers)(four numbers) or (seven numbers)
+#phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) ? true : false
