ebd2517599ab25f262326a5d5be2fb13749937c3
Wayne Buxton <waynebuxton2184@yahoo.com>
2018-01-06 00:34:31 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,21 +1,35 @@
 require "pry"
 def starts_with_a_vowel?(word)
 
-  binding.pry
+  if /\A[aeiouAEIOU]/.match(word)
+    true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+  text.scan(/\w+ing/)
 
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if (/^[A-Z].*\W$/).match(text)
+    true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
-
+    #binding.pry
+  if phone.scan(/\d{1,10}/).join.length == 10
+    true
+  else
+    false
+  end
 end
