1fe70f95529fc8fd285560ef5ec6f8d15e7fa6df
Sam Kalum <stkalum@gmail.com>
2018-01-09 00:36:28 -0500

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,21 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+  word.match(/^[aeiouAEIOU]\w*/) != nil
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\w*ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/^[A-Z].*\W$/) != nil
 end
 
 def valid_phone_number?(phone)
-
+  phone.match(/\D*([2-9]\d{2})(\D*)([2-9]\d{2})(\D*)(\d{4})\D*/) != nil
 end
