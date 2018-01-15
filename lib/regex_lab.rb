7938964ed3c59d71798dabfc895be1d03efd7d0c
Ingrid Wong <yw340@georgetown.edu>
2018-01-15 21:22:23 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,30 @@
+require "pry"
 def starts_with_a_vowel?(word)
-
+  if word.match(/^[AEIOUaeiou]/) == nil
+    false
+  else
+    true
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w*ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z].*[\?\.!]$/) == nil
+    false
+  else true
+  end
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/.?\d{3}.?\d{3}.?\d{4}/) == nil
+    false
+  else true
+  end
 end
