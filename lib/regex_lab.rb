59d08c35b2f8baf927213044a2d6c0e528fec77d
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-01-20 17:57:01 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,29 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\b[aeiouAEIOU]+/)
+    true
+  else 
+    false 
+  end 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b[a-zA-Z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/\b[A-Z]+\W\b/)
+    true
+  else
+    false 
+  end 
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/)
+    true  
+  end 
 end
