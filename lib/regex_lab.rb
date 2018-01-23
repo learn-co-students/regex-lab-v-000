f934b96a56122abaa7055b71d9a18af24074b155
Barbara Choy <barbchoy@gmail.com>
2018-01-23 03:01:55 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,10 @@
 def starts_with_a_vowel?(word)
-
+  word.match('^[aeiouAEIOU].')? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  words = text.split(" ")
+  words.scan('\w+$ing')
 end
 
 def words_five_letters_long(text)
