3f14a26d0e6b366591d70e18014a4d0fa67046fe
Jayon Kim <jpkim921@gmail.com>
2018-01-25 02:05:44 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,12 @@
 def starts_with_a_vowel?(word)
-
+  word.split.each do |test_word|
+    if test_word.match(/^[aeiou|AEIOU]\w+/)
+      puts true
+    elsif  !test_word.match(/^[aeiou|AEIOU]\w+/)
+      # test_word.match(/^[^aeiou|AEIOU]\w+/)
+      puts false
+    end
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
