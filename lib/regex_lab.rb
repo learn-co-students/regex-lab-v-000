efd791bdb230f51534f2769be4550c02db6ccd31
Katrina Brinson <kbrinso@gmail.com>
2018-01-29 18:49:24 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,13 +15,25 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-
+  if text.scan(/\b\w{5}\b/i) == nil
+    return false
+  else
+    return text.scan(/\b\w{5}\b/i)
+  end
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/\A[A-Z]+\p{Any}+\p{P}\z/) == nil
+    return false
+  else
+    return true
+  end
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/(\D[0-9]{3}|[0-9]{3})+(\D[0-9]{3}|[0-9]{3})+(\D[0-9]{4}|[0-9]{4})$/) == nil
+    return false
+  else
+    return true
+  end
 end
