5a5c81feb4cffad61d5a1bf2acb470c9629a35c8
Michael St. Marie <michael.landrum.st.marie@gmail.com>
2018-01-30 16:40:40 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -25,7 +25,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
   end
 
 def valid_phone_number?(phone)
-if /[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})\s*$/.match(phone)
-   true
-end
+ !!phone.match(/[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})\s*$/)
+
+
 end
