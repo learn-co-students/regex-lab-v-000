44af5d5c6ddee3c21aa0a7dd763ed5e77c0a4c06
Harley Rayner <harleymteden@gmail.com>
2018-02-08 21:33:46 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -13,17 +13,27 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/\w+ing/)
+  text.scan(/\bun\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b+\w\w\w\w\w\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.scan(/\b[A-Z]\w+\b[.,!]/).size != 0
+    true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
+
+if phone.scan(/\d|\W\d{10}/).size == 10
+  true
+else
+  false
+end 
 
 end
