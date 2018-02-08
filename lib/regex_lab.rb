0f0ceb5418f3ac63cf02f0daa208a19e1fb302e1
Harley Rayner <harleymteden@gmail.com>
2018-02-08 03:15:19 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,11 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+  if word[0].match(/[aeiou]/) != nil
+    true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
