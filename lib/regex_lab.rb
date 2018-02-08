932e305bfa81accdb149219c2f964e1ffe81d2ae
Harley Rayner <harleymteden@gmail.com>
2018-02-08 04:54:42 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,15 +1,19 @@
 require 'pry'
 
+# def starts_with_a_vowel?(word)
+#   if word[0].match(/[aeiouAEIOU]/) != nil
+#     true
+#   else
+#     false
+#   end
+# end
+
 def starts_with_a_vowel?(word)
-  if word[0].match(/[aeiou]/) != nil
-    true
-  else
-    false
-  end
+  !!word[0].match(/[aeiouAEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\w+ing/)
 end
 
 def words_five_letters_long(text)
