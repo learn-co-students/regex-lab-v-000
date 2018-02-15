2f6121fa0fb2fd388b854c979083d19e273f532d
sgibson47 <sgibson47@gmail.com>
2018-02-15 16:30:56 -0600

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.match(/\d{10}|[(]\d{3}[)]\d{3}[-]\d{4}\z|\d{3}\s\d{3}\s\d{4}|[(]\d{3}[)]\d{7}/) ? true : false
+  phone.match(/\d{10}\z|[(]\d{3}[)]\d{3}[-]\d{4}\z|\d{3}\s\d{3}\s\d{4}\z|[(]\d{3}[)]\d{7}\z/) ? true : false
 end
