85253d19af3187ae9760ecd0a79bc09839e2b307
Harleigh Abel <harleighabel@gmail.com>
2018-02-15 23:51:41 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,34 @@
 def starts_with_a_vowel?(word)
-  !!(word =~ /\b[aeiou]/i)
+  if word.match(/\b[aeiou]/i) 
+    true
+  else
+    false 
+  end
+end  
+  
+def words_starting_with_un_and_ending_with_ing(word)
+  word.scan(/\bun\w+ing\b/i) 
+end  
+
+def words_five_letters_long(word)
+  word.scan(/\b[a-z]{5}\b/i)
+end  
+
+def first_word_capitalized_and_ends_with_punctuation?(string)
+  if string.match(/(^[A-Z]).*([!?.,:;]\z)/)
+    true
+  else
+    false
+  end  
+  
 end
 
-def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/\bun\w+ing\b/)
-end
+def valid_phone_number?(phone_number)
+  if phone_number.match(/([0-9]{3})\W*([0-9]{3})\W*([0-9]{4})/)
+    true
+  else
+    false
+  end 
+  
+end  
 
-def words_five_letters_long(text)
-  text.scan(/\b\w{5}\b/)
-end
-
-def first_word_capitalized_and_ends_with_punctuation?(text)
-  !!(text =~ /^[A-Z].*[\p{P}]$/)
-end
-
-def valid_phone_number?(phone)
-  !!(phone =~ /([0-9]{3})\W*([0-9]{3})\W*([0-9]{4})/)
-end
