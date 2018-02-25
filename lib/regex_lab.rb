9366af961c649a9f3d9caa4b30173fe9e81165d3
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-02-25 20:15:46 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,32 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/^[AEIOUaeiou]/)
+    return true
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/[Uun][a-zA-Z]*ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b[A-Za-z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z].*[\?\.\!]$/)
+    return true
+  else
+    return false
+  end
 end
 
 def valid_phone_number?(phone)
+  if phone.match(/\d{11,}|[^\d\(\)\s-]/)
+    return false
+  else
+    return true
+  end
 
 end
