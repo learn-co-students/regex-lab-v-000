42df975f86b5ac5abf3f17ba389bf3c76d360bc3
github username <github email address>
2018-02-26 16:39:43 -0500

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,22 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+  word.scan(/\w/)[0].match(/[aeiouAEIOU]/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text_array = text.split(" ")
+  text_array.select {|word| word if word[0..1] == "un" && word[-3..-1] == "ing"}
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text[0].match(/[A-Z]/) && text[-1].match(/[.,!?;:]/) ? true : false
 end
 
 def valid_phone_number?(phone)
-
+  phone.scan(/\d/).length == 10
 end
