20cb3ba837aa10470e9a179cad367ecbc1c1d8e6
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-03-10 17:07:22 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,4 +16,5 @@ end
 
 def valid_phone_number?(phone)
   phone.match(/\d{10}|\d{3}\s\d{3}\s\d{4}|\(...\)\d{7}|\(...\)\d{3}\-\d{4}/) ? true : false
+  # phone.match(/[0-9]{3}\W?[0-9]{3}\W?[0-9]{4}/) ? true : false
 end
