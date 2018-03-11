a48b9fb10a5f83c9f0856dbe9f9f6744f24f1619
Travis Williamson <travywill@gmail.com>
2018-03-11 16:23:14 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,37 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\b[aeiouAEIOU]/)
+    true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun[^\s]*ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b[^\s]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/\A[A-Z].*[^a-zA-Z\d\s]\z/)
+    true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/[\d]{10}/)
+    true
+  elsif phone.match(/[(][\d]{3}[)][\d]{3}-[\d]{4}/)
+    true
+  elsif phone.match(/[\d]{3}\s[\d]{3}\s[\d]{4}/)
+    true
+  elsif phone.match(/[(][\d]{3}[)][\d]{7}/)
+    true
+  else
+    false
+  end
 end
