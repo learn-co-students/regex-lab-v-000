4beebfedcc5a50216075fe5e11da449ffc2658bb
David Markel <davidgmarkel@gmail.com>
2018-03-16 16:45:51 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,13 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/^[aeiouAEIOU]/)
+    true
+  else 
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/(^un) (ing$)/)
 end
 
 def words_five_letters_long(text)
@@ -11,7 +15,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/[A-Z]?/)
 end
 
 def valid_phone_number?(phone)
