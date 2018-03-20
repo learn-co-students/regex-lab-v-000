5ebcc192e0ac8b96908d385ac30f345d4e9f793b
Brett Weisberg <weisberg.brett@gmail.com>
2018-03-20 01:44:49 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,23 @@
-def starts_with_a_vowel?(word)
 
+
+def starts_with_a_vowel?(word)
+  if  word.match(/[aeiou]/)
+      true
+    else if word.match /[b-df-hj-np-tv-z]/
+      false
+    end
+  end
 end
 
+
+
 def words_starting_with_un_and_ending_with_ing(text)
+  text.scan(/un+ing/)
 
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\w+un/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
@@ -15,5 +25,9 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
+  phone_number_break = phone.scan(/(\d+)-(\d+)-(\d+)/)
 
+  phone_number_break.each do |numbers|
+  if numbers[0].is_numeric? == numbers[1].is_numeric? == numbers[0].is_numeric?
+  end
 end
