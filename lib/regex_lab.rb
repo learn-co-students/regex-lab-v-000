2208004c34ec2c9003fc37d5f847761b1a26ff72
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-03-30 05:45:36 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def starts_with_a_vowel?(word)
-  word.match(/\b[aeiou]/i) == nil ? false : true
+  word.match(/\b[aeiou]/i) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -13,16 +13,18 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  count = 0;
+  #My initial bad answer.  Tried to handle double quotes (ex. He said "yadda yadda".) but
+  # that is actually really complicated
   # scan will return array of matches.  So we add those all up to make sure there is no extra
-  # stuff in text that is outside of the pattern.  Must be a better way!   wish for /g
-  text.scan(/^[A-Z]+[^.!?]*[.!?"]+/) { | sentence | count += sentence.length}
-  count == text.length ? true : false
+  # stuff in text that is outside of the pattern.  Must be a better way!
+  #count = 0;
+  #text.scan(/^[A-Z]+[^.!?]*[.!?"]+/) { | sentence | count += sentence.length}
+  #count == text.length ? true : false
+  #  better answer for most cases (now I understand $)
+  text.match(/^[A-Z].*[.!?"]$/) ? true : false
 end
 
 #note: doesn't really work all the time.  Ex. (8085551212 is ok.   Not sure how to say if you see 1st (, you need to match the closing )
 def valid_phone_number?(phone)
   phone.match(/(\d\s)?\(?\d{3}\)?[\s-]?\d{3}[\s-]?\d{4}\z/) == nil ? false : true
 end
-
-first_word_capitalized_and_ends_with_punctuation?("Did you hear about the guy whose whole left side was cut off? He's all right now")
