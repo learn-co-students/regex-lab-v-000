0b23bc98b5330b92b639bb417e1a14b493251356
Dale Terrell <tragicid@gmail.com>
2018-05-01 19:13:52 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -2,9 +2,9 @@ require "pry"
 def starts_with_a_vowel?(word)
   if word.match(/\b[aeiouyAEIOUY]/)
 true
-else
-  false
-end
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
