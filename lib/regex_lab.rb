51e5b565476dad46846dfa726454fcec3060672c
Dale Terrell <tragicid@gmail.com>
2018-05-02 01:25:20 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -8,17 +8,17 @@ true
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\w*ing\b/)
 end
 
 def words_five_letters_long(text)
-  wordss.grep(/^\w{5}\s/)
+  text.scan(/\w(5)\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  !!text.match(/^[A-Z].*\W$/)
 end
 
 def valid_phone_number?(phone)
-#  \(?\d{3}\)?[- ]\d{3}-\d{4}
+  phone.scan(/\(?\d{3})\(\d{3-4})/)
 end
