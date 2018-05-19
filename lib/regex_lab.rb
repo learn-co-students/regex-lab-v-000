1466f5a1720afa74dee30ca5ba73c4d939645d15
Jebediah Elliott <jebediahelliott@gmail.com>
2018-05-19 03:29:09 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,17 +3,17 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/^[A-Z].+\W\z/) ? true : false
 end
 
 def valid_phone_number?(phone)
-
+  phone.scan(/[0-9]{10}/) ? true : false
 end
