414411db7d98ecc97576c370d83dd2c227e5c72e
Elizabeth Jimenez <lizliz123@gmail.com>
2018-05-19 04:57:22 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,32 +1,19 @@
 def starts_with_a_vowel?(word)
-  if word.match(/^[aeiouAEIOU]+\w/) then
-      return true
-    else
-      return false
-    end
+  word.match(/\b[aeiou]/i) ? true : false
 end
 
-
 def words_starting_with_un_and_ending_with_ing(text)
-text.scan(/un+\w+ing\b/)
+text.scan(/un+\w+ing/i)
 end
 
 def words_five_letters_long(text)
-text.scan(/\b\w{5}\b/)
+text.scan(/\b[a-z]{5}\b/i)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-if text.match(/^[A-Z].+[\.!?]$/)then
-  return true
-else
-  return false
-end
+text.match(/^[A-Z].+\.$/) ? true : false
 end
 
 def valid_phone_number?(phone)
-  if phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) then
-      return true
-    else
-      return false
-    end
-  end
+phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/)  ? true : false
+end
