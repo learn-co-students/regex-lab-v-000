b3001c9e675fa550b1f86fdc23d035441bc5c305
jeremy.zinger@gmail.com <jeremy.zinger@gmail.com>
2018-05-23 21:49:17 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,15 +1,19 @@
 def starts_with_a_vowel?(word)
-   if word.match(/\[w+a || w+e || w+i || w+o || w+u]/)
+   if /\b[aeiouAEIOU]/.match(word)
      true
+   else
+     false
    end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  
+
 end
 
 def words_five_letters_long(text)
-
+   if text.grep(/^\w{5}\s/)
+     return text.grep
+   end
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
