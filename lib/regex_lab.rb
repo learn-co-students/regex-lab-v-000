b44a989839e76bda83dbceb848e494dcec49addf
Jordan Harris <jharris@wework.com>
2018-06-21 17:27:25 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,29 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+    word.match(/(?i)^[aeiou]\w+/)? true:false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/(?i)\b[a-z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match (/^[A-Z].+\W$/)
+    true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match (/.?\d{3}.?\d{3}.?\d{4}\b/)
+    true
+  else
+    false
+  end
 end
