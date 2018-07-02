91ece98ccabc45c20212e1c69cbc2bee596f23d9
Bailey Sullivan <bailey.rose.sullivan@gmail.com>
2018-07-02 01:19:39 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,22 +1,26 @@
 def starts_with_a_vowel?(word)
-  if word.match(/[aeiouAEIOU] \w*/)
-    return true 
+  if word.match(/\A[aeiou]/i) == nil
+    return false
   else
-    return FALSE
+    return true
   end
 end
 
 
 def words_starting_with_un_and_ending_with_ing(text)
-  
+  text.scan(/un\w*\wing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.scan(/^[A-Z]\w*\W\b/) == nil 
+    return false
+  else
+    return true 
+end
 end
 
 def valid_phone_number?(phone)
