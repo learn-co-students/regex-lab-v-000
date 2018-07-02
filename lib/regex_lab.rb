36682fb17740106acd5845ab055bbeffe5dea05e
Bailey Sullivan <bailey.rose.sullivan@gmail.com>
2018-07-02 01:58:46 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,14 +16,14 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.scan(/^[A-Z].*\W\$/) == nil
-    return false 
-  else
-    return true
+  if text.match(/^[A-Z].*\.$/) == nil
+  return false
+else
+  return true
 end
 end
 
 def valid_phone_number?(phone)
-
+  
 end
 
