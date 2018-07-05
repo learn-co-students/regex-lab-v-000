421ac64831602cedcc9d7c071484f7766d9c2201
Niusha H <niusha.hadziomerovic@gmail.com>
2018-07-05 20:00:23 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,6 +16,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  !!phone.match(/((\d{3}).(\d{3}).(\d{4}))|([0-9]{10})|(^[0-9]{0,8})|(^[0-9]{11,})/)
-  # !phone.match(/([0-9]{0,8})||([0-9]{11,})*([A-Za-z]{0})*/)
+  !!phone.match(/((\d{3}).((\d{3}).([0-9]{4})|(\d{7}))\b)|(\d{10})|([0-9]{10})/)
 end
