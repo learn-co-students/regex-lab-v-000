efdd0391d1ac5eb287354de3544c11d95e6a83df
Joann Pan <joannlpan@gmail.com>
2018-07-14 19:12:21 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,30 @@
+require "pry"
 def starts_with_a_vowel?(word)
-
+   if word.scan(/^[aeiouAEIOU]/).length > 0
+   true
+   else false
+   end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/\b[un][A-za-z]*[ing$]\b/)
 end
 
 def words_five_letters_long(text)
-
+   text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    if text.scan(/^[A-Z].*[.?!]$/) == [text]
+      true
+    else
+      false
+    end
 end
 
-def valid_phone_number?(phone)
-
+def valid_phone_number?(number)
+ if number.scan(/^\D?(\d{3})\D?(\d{3})\D?(\d{4})$/).length > 0
+   true
+ else false
+ end
 end
