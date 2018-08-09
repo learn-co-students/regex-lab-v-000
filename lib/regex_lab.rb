6d5cd0a2419ebeda5ebe98886967b8921ff45a3c
Brad McCollum <btmcco8@gmail.com>
2018-08-09 20:36:02 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-  !!(word.match(/\b[aeiouAEIOU]\w+\b/))
+  !!(word.match(/\b[aeiou]/i)) #returns true if the word begins with a vowel
 end
 
-def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/\b+un+\w+ing+\b/)
+def words_starting_with_un_and_ending_with_ing(text) #creates an array of all words starting with 'un' and ending with 'ing'
+  text.scan(/\bun+\w+ing\b/)
 end
 
-def words_five_letters_long(text)
- text.scan(/\b+\w{5}+\b/)
+def words_five_letters_long(text) #creates an array of all five letter words
+ text.scan(/\b\w{5}\b/)
 end
 
-def first_word_capitalized_and_ends_with_punctuation?(text)
+def first_word_capitalized_and_ends_with_punctuation?(text) # returns true if the first word is capitalized and the string ends with punctuation
   !!(text.match(/^[A-Z]/) && text.match(/\W$/))
 end
 
-def valid_phone_number?(phone)
+def valid_phone_number?(phone) #returns true if the phone number format is valid, regardless of format
   !!(phone.match(/(\d{3}?)+\W?+(\d{3})+\W?+(\d{4})/))
 end
