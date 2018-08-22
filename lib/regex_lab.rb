5703150b321a946a8b38c8d5e60fed77f13fc53d
Nick Romans <nicholas.romans@gmail.com>
2018-08-22 18:29:59 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,28 @@
 def starts_with_a_vowel?(word)
-
+if word.match(/\b[aeiou]/i)
+  true
+else
+  false 
+end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+text.scan(/\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+ if text.match(/^[A-Z].*\W$/)
+ true
+ else false
 end
-
+end
 def valid_phone_number?(phone)
+if phone.match(/[(]?\d{3}[),\s]?\d{3}[\W,\s]?\d{4}?/)
+  true
+end
 
 end
