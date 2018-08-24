d42a9983c4f3626c291c56f303c5f38968b43c31
Ashley Wong <ashwong04@gmail.com>
2018-08-24 13:46:28 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,7 @@
 def starts_with_a_vowel?(word)
-   if word.scan(/\A+[aeiou]/)
+   if word.scan(/\A[aeiou]/)
     return true
-  elsif word.scan(/\A+[^aeiou]/)
+  elsif word.scan(/\A[^aeiou]/)
     return false
   end
 end
@@ -11,7 +11,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(//)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
