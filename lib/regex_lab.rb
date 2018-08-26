a3ec5b6fd095b8e98d0e8012f741d9d324216dd8
Ashley Wong <ashwong04@gmail.com>
2018-08-26 02:19:10 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,11 +11,15 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  #text.scan(/\.{3}/)
+  text.scan(/\b[a-z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  
+  if text.match(/^[A-Z].*\W$/)
+    return true
+  else
+    return false
+  end
 end
 
 def valid_phone_number?(phone)
