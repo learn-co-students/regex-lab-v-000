3907273e8af095bd54cfe4a90a603e37ff5730f7
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-08-28 15:15:14 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,9 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/[aeiouAEIOU]+\w/)
+  return true
+else
+  return false
+end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -7,7 +11,8 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-
+text.grep(/^\w{5}\s/)
+return true
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
