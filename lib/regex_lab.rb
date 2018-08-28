747579c9e9b68bfb1d03d49d559515605f44cb5b
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-08-28 17:49:18 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-  if word.match(/[aeiouAEIOU]+\w/)
+  if word.match(/^[aeiouAEIOU]+\w/)
   return true
 else
   return false
@@ -7,18 +7,26 @@ end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un+\w+ing/)
 end
 
 def words_five_letters_long(text)
-text.grep(/^\w{5}\s/)
-return true
+  text.scan(/\b\w{5}\b/)
+
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z].+[.!?]$/) 
+    return true
+  else
+    return false
+  end
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7}))/)
+    return true
+  else
+    return false
+  end
 end
