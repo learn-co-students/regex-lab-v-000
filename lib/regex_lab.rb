98ae4b3f012df11fa58ffbdf94272a603f4fa0aa
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-08-30 12:42:39 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,13 @@
 def starts_with_a_vowel?(word)
- if /\A[aeiouAEIOU]/.match(word)
-   true 
- else
-   false
- end
+  /\A[aeiouAEIOU]/.match(word) != nil
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w*ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
