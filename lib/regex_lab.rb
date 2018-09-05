fa3c544fe18cb35c76b7743d6d2ce7f6f8d14e62
Stef Claus <stef.claus@gmail.com>
2018-09-05 12:39:56 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,10 @@
 def starts_with_a_vowel?(word)
-
+  array = word.scan(/\b\w+[aeiouAEIOU]/)
+  if array.nil?
+    false 
+  else
+    true 
+  end 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
