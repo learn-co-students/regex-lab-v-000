9fdd743cf4631f7cabca9d3014fbcdeefe0ecf6e
Amil Masic <amil.masic@gmail.com>
2018-09-05 23:26:44 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,37 @@
+# require "pry"
 def starts_with_a_vowel?(word)
-
+  # binding.pry
+  if word.match(/\b[aeiou]/i)
+    true
+  else
+     false
+    end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+   # # text.scan(/\w^un+\w$ing/)
+   text.scan(/un\w+ing/)
+
 
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b[a-z]{5}\b/i)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z].+\.$/)
+    true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
+  if phone.match (/^[(]?\d{3}[)]?[\s]?\d{3}[\s-]?\d{4}$/)
+    true
+  else
+    false
+  end
 
 end
