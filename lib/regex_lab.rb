119ce9cc16e597bf9b570367c2df00cde79c4cd1
Rachel Feldman <rachel.a.f.@Rachels-MacBook-Pro.local>
2018-09-16 11:40:44 -0400

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,20 @@
 def starts_with_a_vowel?(word)
-
+    !word[0].match(/[aeiou]/i).nil?
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/\w+un/) + text.scan(/\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+    text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    !text.scan(/\A[A-Z]/).empty?  && !text.scan(/\W\z/).empty?
 end
 
 def valid_phone_number?(phone)
-
+    digits = phone.scan(/\d/)
+    return true if digits.length === 10
 end
