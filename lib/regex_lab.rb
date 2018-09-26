ada811b6c1f54b1106effa884ec03c433ddfccee
Amanda Murphy <helloamandamurphy@gmail.com>
2018-09-26 19:04:19 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,21 @@
 def starts_with_a_vowel?(word)
-
+  word.scan(/\w^[aeiou]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\w^(un)\$(ing)/)
 end
 
 def words_five_letters_long(text)
-
+  text.grep(/\w{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.grep(/\w^/)
+  
 end
 
 def valid_phone_number?(phone)
-
+  phone.match(/\d{10}/)
+#phone.match(/\d{3}\-\d{3}\-\d{4}/)
 end
