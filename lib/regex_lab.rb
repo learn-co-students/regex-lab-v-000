aa489714898d683c3f5a331805d71efce03559ef
Amanda Murphy <helloamandamurphy@gmail.com>
2018-09-27 15:40:01 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,18 +3,17 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/\b^(un)\$(ing)/)
+  text.scan(/\b[un]\w*[ing]\b/)
 end
 
 def words_five_letters_long(text)
-  text.grep(/\w{5}/)
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.grep(/\w^/) ? true : false
+  text.match(/^[A-Z].*[\.!?]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
-  phone.match(/\d{10}/) ? true : false
-#phone.match(/\d{3}\-\d{3}\-\d{4}/)
+  phone.match(/\W?\d{3}\W?\d{3}\W?\d{4}/) ? true : false
 end
