8fc355bb5c57baa163621a820e662916b97c4a45
Vince Andrew Pimentel <contact@vincep.me>
2018-10-10 20:54:09 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,35 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\b[aieouAEIOU]/)
+    return true
+  elsif word.match(/\b[^aieouAEIOU]/)
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\w*ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/\A[A-Z].*\W\z/)
+    return true
+  elsif text.match(/\A[a-z].*\W\z/)
+    return false
+  elsif text.match(/\A[A-Z].*\w\z/)
+    return false
+  elsif text.match(/\A[a-z].*\w\z/)
+    return false
+  end
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/[\(| ]?\d{3}[\)| ]?\d{3}[-| ]?\d{4}\b/)
+    return true
+  else
+    return false
+  end
 end
