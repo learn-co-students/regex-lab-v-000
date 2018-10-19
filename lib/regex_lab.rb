b88e776f0af1eeb109a550679bc0c697bfc5e26d
Malcome Fein <mfein90@gmail.com>
2018-10-19 09:22:26 -0400

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,35 @@
 def starts_with_a_vowel?(word)
-
+  if /^[^aeiou]/i.match(word)
+    return false
+  else
+    return true
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+ text.scan(/un\w*ing/)
 end
 
 def words_five_letters_long(text)
-
+ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+   text.match(/^[A-Z].+[\.!?]$/) ? true : false
+  # if /^[A-Z].*[.!?,;:]$/i.match(text)
+  #   return true
+  # # elsif /^[^A-Z].*[.!?,;:]$/i.match(text)
+  # #   return false
+  # else
+  #   return false
+  # end
 end
 
 def valid_phone_number?(phone)
-
+   phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) ? true : false
+  # if phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/)
+  #   return false
+  # else
+  #   return true
+  # end
 end
