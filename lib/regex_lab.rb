d058b283fedfb4ec66ad9536d2d6fbe26d7fe9ea
Lois Lee <lois.lee@me.com>
2018-10-24 20:59:28 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,23 +1,24 @@
+require 'pry'
+
 def starts_with_a_vowel?(word)
-  if word.match(/^[aeiou]/i)
-    true
-  else
-    false
-end
+  word.match(/\b[aeiouAEIOU]/)? true : false
 end
 
-def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/\b+un\w+ing/)
+
+def words_starting_with_un_and_ending_with_ing(word)
+  word.scan(/\b[aeiouAEIOU]+\w*ing\b/)
 end
 
-def words_five_letters_long(text)
-  text.scan(/\b\w{5}\b/)
+def words_five_letters_long(word)
+  word.scan(/\b\w{5}\b/)
 end
 
-def first_word_capitalized_and_ends_with_punctuation?(text)
-    text.match(/^[A-Z].+[.?!]$/)? true : false
+
+def first_word_capitalized_and_ends_with_punctuation?(string)
+  string.match(/^[A-Z].+\W$/)? true : false
 end
 
-def valid_phone_number?(phone)
-  phone.match(/1?[\s-]?\(?(\d{3})\)?[\s-]?\d{3}[\s-]?\d{4}/)
+def valid_phone_number?(string)
+  string.match(/\d{3}.*\d{3}.*\d{4}/)? true : false
+
 end
