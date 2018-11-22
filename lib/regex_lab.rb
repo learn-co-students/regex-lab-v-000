eabbe5db4e1ddcea330db47000128f399ada4f2d
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-11-22 02:26:06 +0000

    wip

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,11 @@
-def starts_with_a_vowel?(word)
+require "pry"
 
+def starts_with_a_vowel?(word)
+  !!word.match(/\A[aeiou]/i)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun[\S]+ing\b/)
 end
 
 def words_five_letters_long(text)
