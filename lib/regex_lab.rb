28f8ac7a475d5a4bb13b876f085500b2fe69142b
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-11-22 21:00:45 +0000

    done

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -13,9 +13,9 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  !!text.match(/\A[A-Z](.*)[.]\z/)
 end
 
 def valid_phone_number?(phone)
-
+ !!phone.match(/(\(?)(\d{3})(\s|\)|-?)(\d{3})(\s|-?)(\d{4})/)
 end
