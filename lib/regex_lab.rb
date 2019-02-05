f9efb2841c42dae425e274c17a2012ad9b502a78
lcorr8 <l.corr.v@gmail.com>
2019-02-05 13:20:55 +0100

    Done 2

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,33 +1,19 @@
-
-require 'pry'
-
 def starts_with_a_vowel?(word)
-  if word.match(/^[aeiouAEIOU]\w+/) 
-     true
-  else 
-     false
-  end   
-   #word.match(/^[aeiouAEIOU]\w+/) ? true:false
+  !!word.match(/^[aeiouAEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-text.scan(/un\w+ing\b/)
+ text.scan(/\bun[a-z]*ing\b/)
 end
 
 def words_five_letters_long(text)
-  text.scan(/\b\w{5}\b/)
+ text.scan(/\b[a-zA-Z]{5}\b/)
 end
-
-
-
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-text.match(/^[A-Z].+[\.!?]$/) ? true : false
-
+ !!text.match(/\b[A-Z]{1}[a-z]*[,\.!?]{1}/)
 end
-
 
 def valid_phone_number?(phone)
-  phone.match(/\(*[0-9]{3}.?[0-9]{3}.?[0-9]{4}\b/) ? true : false
+ !!phone.match(/[\(]?\d{3}[\-\)\s]?\d{3}[\-\s]?\d{4}/)
 end
-
