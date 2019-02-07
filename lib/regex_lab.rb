b2e5a4cdbb729e39f6b92ace4361f4ed3e0bd390
Dan Suarez <suarez.daniel101@gmail.com>
2019-02-07 19:00:13 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,9 +11,9 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  !!text.match(/\b[A-Z]\w+\b\S\s/)
 end
 
 def valid_phone_number?(phone)
-
+  phone.match(/\b[0-9]\w+\b/)
 end
