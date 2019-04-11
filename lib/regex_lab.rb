b6aab5aaee9ff59afde7f2c37b84bfc898b345f9
alejandra cantu <alejandra.cantu13@gmail.com>
2019-04-11 02:25:46 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,6 @@
 def starts_with_a_vowel?(word)
-
+if word.scan(/\A[aAeEIiOoUu]/) then true
+end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -15,5 +16,6 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
+
 
 end
