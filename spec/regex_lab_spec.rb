e6b63101ebc057e0110f5a042f45ba0bcd7c604d
alejandra cantu <alejandra.cantu13@gmail.com>
2019-04-13 01:05:24 +0000

    Done.

--- spec/regex_lab_spec.rb
+++ spec/regex_lab_spec.rb
@@ -23,7 +23,7 @@ describe "Working with Regular expressions" do
   describe "#words_starting_with_un_and_ending_with_ing" do
     it "returns an array with the words starting with 'un' and ending with 'ing'" do
       words_string = "unassuming ambiguous understanding pomp circumstance uninteresting uncompromising grouchy corollary"
-      
+
       expect(words_starting_with_un_and_ending_with_ing(words_string).count).to eq(4)
       expect(words_starting_with_un_and_ending_with_ing(words_string)).to include("understanding")
       expect(words_starting_with_un_and_ending_with_ing(words_string)).not_to include("pomp")
@@ -67,8 +67,16 @@ describe "Working with Regular expressions" do
     end
 
     it "returns false for invalid phone numbers, regardless of formatting" do
-      valid_numbers = ["28894546", "(718)891-13135", "234 43 9978", "(800)IloveNY"]
-      expect(valid_numbers.all? { |number| valid_phone_number?(number) }).to be(false)
+      ^\s*                #Line start, match any whitespaces at the beginning if any.
+(?:\+?(\d{1,3}))?   #GROUP 1: The country code. Optional.
+[-. (]*             #Allow certain non numeric characters that may appear between the Country Code and the Area Code.
+(\d{3})             #GROUP 2: The Area Code. Required.
+[-. )]*             #Allow certain non numeric characters that may appear between the Area Code and the Exchange number.
+(\d{3})             #GROUP 3: The Exchange number. Required.
+[-. ]*              #Allow certain non numeric characters that may appear between the Exchange number and the Subscriber number.
+(\d{4})             #Group 4: The Subscriber Number. Required.
+(?: *x(\d+))?       #Group 5: The Extension number. Optional.
+\s*$                #Match any ending whitespaces if any and the end of string.
     end
   end
 
