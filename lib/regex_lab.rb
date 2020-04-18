2899cbbea48baa054f4ef5d765160aabbde1a6cc
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-04-18 17:07:57 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,31 +1,27 @@
 def starts_with_a_vowel?(word)
-  if word.match(/\b[AEIOUaeiou]/) == nil
-    false
-  else
-    true
-  end
-end
-
-def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/\bun\S*ing\b/)
-end
-
-def words_five_letters_long(text)
-  text.scan(/\b\w{5}\b/)
-end
-
-def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/^[A-Z].*[?.!]$/) != nil
+  if word.match(/\b[aeiouAEIOU]/)
     true
   else
     false
   end
 end
 
-def valid_phone_number?(phone)
-  if phone.match(/^(\d{10})$/) != nil || phone.match(/^\W(\d{3})\W(\d{3})\W(\d{4})$/) != nil || phone.match(/^(\d{3})\W(\d{3})\W(\d{4})/) != nil || phone.match(/^\W(\d{3})\W(\d{7})$/) != nil
+def words_starting_with_un_and_ending_with_ing(word)
+  word.scan(/\bun\w*ing\b/)
+end
+
+def words_five_letters_long(word)
+  word.scan(/\b\w{5}\b/)
+end
+
+def first_word_capitalized_and_ends_with_punctuation?(string)
+  if string.match(/\A[A-Z].*[.?!]\z/)
     true
   else
     false
   end
+end
+
+def valid_phone_number?(number)
+  number.scan(/[^\D]/).length == 10
 end
