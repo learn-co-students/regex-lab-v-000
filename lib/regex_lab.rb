a2ead39179559497ac6f3af70e4826dae902fa10
Joel Croitoru <jcroitoru@gmail.com>
2020-06-18 14:32:23 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,31 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/^[aeiouAEIOU]\w+/)
+    return true
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un[a-z]*ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b[a-zA-Z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/[A-Z]\w*[.,:;]/)
+    return true
+  else
+    return false
+  end
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/\b[0-9]{10}\b|\([0-9]{3}\)[0-9]{3}-[0-9]{4}|\([0-9]{3}\)[0-9]{7}|[0-9]{3} [0-9]{3} [0-9]{4}/)
+    return true
+  else
+    return false
+  end
 end
