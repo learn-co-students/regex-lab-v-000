d7da82d21e214e67aec2d5505b196d77a1c82a55
danpaslawsky <danpaslawsky@gmail.com>
2020-12-28 17:51:36 -0800

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,22 @@
-def starts_with_a_vowel?(word)
 
+
+def starts_with_a_vowel?(word)
+!!word.capitalize.match(/\A+[AEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+text.scan(/un+\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+text.scan(/\b\w[a-z]{4}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+# Regexp.new(/^[A-Z].*[a-zA-Z][.]$/)? true : false
+!!text.match(/^[A-Z].+\W$/)
 end
 
 def valid_phone_number?(phone)
-
+phone.scan(/\d/).count == 10
 end
